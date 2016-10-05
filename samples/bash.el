a4e85f34423f9f885b9fd1f4a1c44bca093882dd
George Lester <glester491@gmail.com>
2016-10-04 18:45:30 -0700

    Getting closer to bash parsing

--- samples/bash.el
+++ samples/bash.el
@@ -1,55 +1,59 @@
 // any unicode alphanumeric
-unquotedString [\\p{L}\\p{Nd}_/\\.]+
+unquotedString [-:_./\\\\\\p{L}\\p{Nd}]+
 
 interpolatedString [\"].*[\"]
 literalString '.*'
 
-whitespace [\\s\r\n]+
+//assignment [\n;][-:_./\\\\\\p{L}\\p{Nd}]+=
+
+whitespace [ \t\r]+
 comment #.*\n
 
 %%
 
-{comment}	{COMMENT}
+{comment}		{COMMENT}
 
-"|"			{PIPE}
-">>" 		{PIPE_APPEND}
-">" 		{PIPE_OVERWRITE}
-"<" 		{PIPE_INPUT}
-"&>"		{PIPE_COMBINED_OVERWRITE}
+"|"				{PIPE}
+">>" 			{PIPE_APPEND}
+">" 			{PIPE_OVERWRITE}
+"<" 			{PIPE_INPUT}
+"&>"			{PIPE_COMBINED_OVERWRITE}
 
-"$"			{VARIABLE}
-"${"		{VARIBLE_LONGFORM}
-"$!"		{VARIABLE_LASTPID}
-"$?"		{VARIABLE_LASTSTATUS}
+"$"				{VARIABLE}
+"${"			{VARIBLE_LONGFORM}
+"$!"			{VARIABLE_LASTPID}
+"$?"			{VARIABLE_LASTSTATUS}
+"$@"			{VARIABLE_ALLARGS}
+"export"		{EXPORT}
+"local"			{VARIABLE_LOCAL}
 
-"&"			{BACKGROUND}
-"$("		{SUBSHELL}
-"("			{PAREN_OPEN}
-")"			{PAREN_CLOSE}
-"{"			{BRACE_OPEN}
-"}"			{BRACE_CLOSE}
-"["			{BRACKET_OPEN}
-"]"			{BRACKET_CLOSE}
-"[["		{EXPRESSION_OPEN}
-"]]"		{EXPRESSION_CLOSE}
-"="			{ASSIGNMENT}
+"&"				{BACKGROUND}
+"$("			{SUBSHELL}
+"("				{PAREN_OPEN}
+")"				{PAREN_CLOSE}
+"{"				{BRACE_OPEN}
+"}"				{BRACE_CLOSE}
+"["				{BRACKET_OPEN}
+"]"				{BRACKET_CLOSE}
+"[["			{EXPRESSION_OPEN}
+"]]"			{EXPRESSION_CLOSE}
+"=" 			{ASSIGNMENT}
 
-"if"		{IF}
-"then"		{THEN}
-"fi"		{FI}
-"while"		{WHILE}
-"do"		{DO}
-"done"		{DONE}
-"for"		{FOR}
-"in"		{IN}
-"function"	{FUNCTION}
-"export"	{EXPORT}
+"if"			{IF}
+"then"			{THEN}
+"fi"			{FI}
+"while"			{WHILE}
+"do"			{DO}
+"done"			{DONE}
+"for"			{FOR}
+"in"			{IN}
+"function"		{FUNCTION}
 
-"*"			{WILDCARD}
-";"			{TERMINATOR}
-","			{SEPARATOR}
-"\\"		{ESCAPE}
-"\\\n"		{ESCAPE_NEWLINE}
+"*"				{WILDCARD}
+";"|"\n"|";\n"	{TERMINATOR}
+","				{SEPARATOR}
+"\\"			{ESCAPE}
+"\\\n"			{ESCAPE_NEWLINE}
 
 {unquotedString}		{STRING}
 {interpolatedString}	{STRING_INTERPOLATED}
@@ -60,5 +64,5 @@ comment #.*\n
 
 // may not be useful,
 //"\""		{QUOTE_INTERPOLATED}
-//"'"			{QUOTE_LITERAL}
+//"'"		{QUOTE_LITERAL}
 %%
